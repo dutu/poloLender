a2bb183595ad45eee13b1e4f564754045f9d39ea
dutu <adrian.clinciu@outlook.com>
2017-06-20 23:00:11 +0300

    0.10.0-beta1

--- server/core/logger.js
+++ server/core/logger.js
@@ -5,7 +5,7 @@ let customLogLevels = _.clone(winston.config.syslog.levels);
 delete customLogLevels.emerg;
 customLogLevels.report = 0;
 let customLogColors = _.clone(winston.config.allColors);
-customLogColors.report = 'blue';
+customLogColors.report = 'cyan';
 winston.addColors(customLogColors);
 let logger = new (winston.Logger)({
 	levels: customLogLevels,
